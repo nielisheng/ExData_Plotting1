2aaedbf7fa9fe23bddb0d72f30a0b0528c0ff756
nielisheng <nielisheng@gmail.com>
2014-07-11 14:55:35 +0800

    no message

---
+++ plot2.R
@@ -0,0 +1,23 @@
+##Read data into hpc
+hpc <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
+
+##subset date frame into hpc2
+hpc2 <- subset(hpc, as.Date(Date, format="%d/%m/%Y")=="2007-02-01" | as.Date(Date, format="%d/%m/%Y")=="2007-02-02")
+
+#conbine Date and Time to a new variable
+hpc2$DateTime = paste(hpc2$Date, hpc2$Time)
+
+x <- as.POSIXlt(hpc2$DateTime, tz="", format="%d/%m/%Y %H:%M:%S")
+y <- as.POSIXlt("01/02/2007 00:00:00", tz="", format="%d/%m/%Y %H:%M:%S")
+
+#get interval of minitues
+hpc2$DTInterval = difftime(x, y, units=c("mins"))
+
+##set graphic device to png
+png(filename="plot2.png", width=480, height=480)
+
+##plot histgram
+plot(hpc2$DTInterval, as.numeric(hpc2$Global_active_power), type="l")
+
+##shut off png device
+dev.off()
