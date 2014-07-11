7b809a027bf2348facd7bb88e67d2b3191061935
nielisheng <nielisheng@gmail.com>
2014-07-11 10:29:19 +0800

    no message

---
+++ plot1.R
@@ -0,0 +1,14 @@
+##Read data into hpc
+hpc <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
+
+##subset date frame into hpc2
+hpc2 <- subset(hpc, as.Date(Date, format="%d/%m/%Y")=="2007-02-01" | as.Date(Date, format="%d/%m/%Y")=="2007-02-02")
+
+##set graphic device to png
+png(filename="plot5.png", width=480, height=480)
+
+##plot histgram
+hist(as.numeric(hpc2$Global_active_power), col="red")
+
+##shut png device
+dev.off()
